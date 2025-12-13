7a16a7d9166ec7e82b7a8d66095b18a638c07405
Teena Pal <palteena08@gmail.com>
2025-12-13 12:59:10 +0530

    fourthcommit

---
+++ index.js
@@ -0,0 +1,30 @@
+//SYNC ->means STEP BY STEP 
+// console.log("A")
+// for(let i=1;i<=1000000000;i++){
+
+// }
+// console.log("B")
+// console.log("C")  //in output first A will return then take some sec of time then B and C return 
+
+
+
+//setTimeout(useCallback,delay)
+// setTimeout(() => { console.log("A"), 2000 })  //2sec
+// setTimeout(() => { console.log("B"), 3000 })  //3sec
+// setTimeout(() => { console.log("C"), 6000 })  //6sec
+
+
+
+// console.log("i will call dominos pizza") //t==2sec
+// console.log("pizza being prepared") //t==5sec
+// console.log("pizza on the way") //t==3sec
+// console.log("pizza delivered") //t==5sec
+// console.log("noe i will eat pizza") //t==4sec
+
+
+///start with t=0
+// setTimeout(()=>{console.log("i will call dominos"),2000}) 
+// setTimeout(()=>{console.log("pizza being prepared"),5000}) 
+// setTimeout(()=>{console.log("pizza on the way"),3000}) 
+// setTimeout(()=>{console.log("pizza delivered"),5000}) 
+// setTimeout(()=>{console.log("now i will eat my pizza"),4000}) 
